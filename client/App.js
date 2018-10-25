603680d663f58b8e823615cb4e66c7fa7b315776
Albin Groen <albin.groen@gmail.com>
2018-10-26 01:14:28 +0200

    Added a basic form to app

--- client/App.js
+++ client/App.js
@@ -1,17 +1,58 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput
+} from "react-native";
 import { white } from "ansi-colors";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { firstName: "", lastName: "", password: "" };
+  }
+
+  handleSubmit() {
+    const { firstName, lastName } = this.state;
+    this.setState({
+      submitted: true,
+      message: `You submitted first-name: ${firstName} and last-name: ${lastName}`
+    });
+  }
+
   render() {
+    const { submitted, message } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>SL-Broke App</Text>
-        <TouchableOpacity onPress={() => console.log("Clicked...")}>
+        <Text style={styles.topText}>SL-Broke App</Text>
+        <TextInput
+          style={styles.inputField}
+          onChangeText={text => this.setState({ firstName: text })}
+          value={this.state.firstName}
+          placeholder="Firstname"
+        />
+        <TextInput
+          style={styles.inputField}
+          onChangeText={text => this.setState({ lastName: text })}
+          value={this.state.lastName}
+          placeholder="Lastname"
+        />
+        <TextInput
+          style={styles.inputField}
+          onChangeText={text => this.setState({ password: text })}
+          value={this.state.password}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity onPress={() => this.handleSubmit()}>
           <View style={styles.button}>
-            <Text style={styles.buttonText}>Click me</Text>
+            <Text style={styles.buttonText}>Submit form</Text>
           </View>
         </TouchableOpacity>
+        {submitted && <Text>{message}</Text>}
       </View>
     );
   }
@@ -24,16 +65,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
+  inputField: {
+    marginTop: 10,
+    padding: 18,
+    color: "#222",
+    width: 375,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    fontSize: 18
+  },
   button: {
-    padding: 15,
-    backgroundColor: "dodgerblue",
-    borderRadius: 4,
-
-    width: 375
+    width: 375,
+    marginTop: 20,
+    backgroundColor: "#222",
+    borderRadius: 5,
+    padding: 16
   },
   buttonText: {
     color: "white",
-    fontSize: 20,
-    textAlign: "center"
+    textAlign: "center",
+    fontSize: 16
   }
 });
