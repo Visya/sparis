8d7e97371357ce9d8bb1010b1d2a2ee43a018136
Maria Verbenko <m.verbenko@gmail.com>
2018-11-14 16:23:57 +0100

    Move inline styles to StyleSheet

--- components/HeaderLeft.js
+++ components/HeaderLeft.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 const HeaderLeft = props => (
   <TouchableOpacity onPress={() => props.navigate('Home', {})}>
-    <View
-      style={{
-        paddingLeft: 16,
-        alignItems: 'center'
-      }}
-    >
-      <Text
-        style={{
-          color: '#fff',
-          fontSize: 18,
-          fontWeight: '600'
-        }}
-      >
+    <View style={styles.Container}>
+      <Text style={styles.Text}>
         Spåris
       </Text>
     </View>
   </TouchableOpacity>
 )
+
+const styles = StyleSheet.create({
+  Container: {
+    paddingLeft: 16,
+    alignItems: 'center'
+  },
+  Text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600'
+  }
+})
 
 export default HeaderLeft
--- components/HeaderRight.js
+++ components/HeaderRight.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 
 const HeaderRight = props => (
-  <View
-    style={{
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingRight: 16
-    }}
-  >
+  <View style={styles.Container}>
     <TouchableOpacity onPress={() => props.navigate('About', {})}>
       <FontAwesome name='question-circle-o' size={32} color='white' />
     </TouchableOpacity>
     <TouchableOpacity onPress={() => props.navigate('InfoTicket', {})}>
-      <View
-        style={{
-          marginLeft: 16
-        }}
-      >
+      <View style={styles.SettingsIcon}>
         <FontAwesome name='gear' size={32} color='white' />
       </View>
     </TouchableOpacity>
   </View>
 )
+
+const styles = StyleSheet.create({
+  Container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingRight: 16
+  },
+  SettingsIcon: {
+    marginLeft: 16
+  }
+})
 
 export default HeaderRight
